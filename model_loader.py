f94b9f7774317a482c9c7f7cf98dea2d02fb94b2
Salo Lacunes <s.lacunes@gmail.com>
2023-02-03 22:20:31 +0100

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -20,7 +20,7 @@ def setup_explainer(settings, hook_fn=None, random_feature=False):
                 continue
             else:
                 model._modules[module_name] = nn.Identity()
-        feature_dim = list(model._modules[settings.layer]._modules.values())[-1].conv2.out_channels
+        feature_dim = list(model._modules[settings.layer]._modules.values())[-1].conv3.out_channels
         model._modules[settings.classifier_name] = nn.Sequential(
             nn.BatchNorm1d(feature_dim),
             nn.Dropout(0.1),
